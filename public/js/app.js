d303cd155db90d2488eb9e9a9430d07b41d8a0cf
RoshanJose7 <roshanstark007@gmail.com>
2020-09-13 20:08:44 +0530

    Update: Contact Page

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,36 @@
 const bm = bodymovin.loadAnimation({
-  container: document.getElementById("hero-img"),
-  renderer: "svg",
-  loop: true,
-  autoplay: true,
-  path: "../img/wash-your-hands.json",
+    container: document.getElementById("hero-img"),
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "../img/wash-your-hands.json",
 });
 
-const reveal = () => {
-  const dropDiv = document.querySelector(".dropDown");
-  const btn = document.querySelector("#drop-btn");
+const errorH1=document.querySelector('main h1')
+const error=document.querySelector('main p')
+const table=document.querySelector('main table')
 
-  btn.addEventListener("click", () => {
-    dropDiv.classList.toggle("revealed");
-  });
-};
+fetch("https://api.rootnet.in/covid19-in/contacts")
+    .then(res=>res.json())
+    .then(data => {
+        if(!data) {
+            errorH1.textContent="Error fetching the API"
+            error.textContent=data.error
+        }
+        else{
+            data.data.contacts.regional.forEach(item=>{
+                const tr=document.createElement('tr')
+                const td1 = document.createElement('td')
+                const td2 = document.createElement('td')
+
+                const loc=document.createTextNode(item.loc)
+                const ph=document.createTextNode(item.number)
+
+                td1.appendChild(loc)
+                td2.appendChild(ph)
+                tr.appendChild(td1)
+                tr.appendChild(td2)
+                table.appendChild(tr)
+            })
+        }
+    })
